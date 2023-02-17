a87e8ed416430ae506266a23875d2dfc86d83301
62830 <willychang2004@gmail.com>
2023-02-17 23:23:32 +0800

    delete comments, solve a bug in prytyfy_pair

--- test_template.cpp
+++ test_template.cpp
@@ -25,39 +25,11 @@ struct Dim<tuple<S,T...>>{
 	enum{val = max((int)Dim<S>::val,(int)Dim<tuple<T...>>::val)};
 };
 
-#define O cout
+#define OSTRM cout
 template<typename T>
 void _print(const T &a,const int idt){
-	O<<a;
+	OSTRM<<a;
 }
-/*
-#define PRYTYFY_DECLARE(U) template<typename ...T>ostream& operator<<(ostream&O,const U<T...> &a); template<typename ...T> void _print(const U<T...> &a,const int idt = 0,typename enable_if< (Dim<U<T...>>::val>1) >::type* = 0);
-
-PRYTYFY_DECLARE(vector)
-PRYTYFY_DECLARE(deque)
-PRYTYFY_DECLARE(set)
-PRYTYFY_DECLARE(multiset)
-PRYTYFY_DECLARE(unordered_set)
-PRYTYFY_DECLARE(map)
-PRYTYFY_DECLARE(unordered_map)
-//template<typename T,typename TT> ostream& operator<<(ostream&O,const pair<T,TT> &a);
-//template<typename ...T>ostream& operator<<(ostream&O,vector<T...> &a);
-//template<typename T,typename TT> void _print(const pair<T,TT> &a,const int idt = 0,typename enable_if< (Dim<pair<T,TT>>::val>1) >::type* = 0);
-//template<typename ...T> void _print(vector<T...> &a,int idt = 0,typename enable_if< (Dim<vector<T...>>::val>1) >::type* = 0);
-PRYTYFY_DECLARE(tuple)
-PRYTYFY_DECLARE(pair)
-
-// M for one-dimention, N for N-dimension
-#define PRYTYFY(U,B,S,E,M,N) template<typename ...T> ostream& operator<<(ostream&O,const U<T...> &a){O<<B; int c=0; for(auto i:a) O<<(c++?S:"")<<M; return O<<E; } template<typename ...T> void _print(const U<T...> &a,const int idt,typename enable_if< (Dim<U<T...>>::val>1) >::type*){cerr<<B; int c=0; int dim = Dim<U<T...>>::val; string sep = S; for(int i=0;i<dim-1;++i) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; for(auto i:a){cerr<<(c++?sep:"");N; } cerr<<E; }
-
-PRYTYFY(vector,"[",", ","]",i,_print(i,idt+1););
-PRYTYFY(deque,"[",", ","]",i,_print(i,idt+1););
-PRYTYFY(set,"{",", ","}",i,_print(i,idt+1););
-PRYTYFY(multiset,"{",", ","}",i,_print(i,idt+1););
-PRYTYFY(unordered_set,"{",", ","}",i,_print(i,idt+1););
-PRYTYFY(map,"{",", ","}",i.first<<":"<<i.second,_print(i.first,idt+1); cerr<<((Dim<map<T...>>::val) > 2?":\n":":"); _print(i.second,idt+1););
-PRYTYFY(unordered_map,"{",", ","}",i.first<<":"<<i.second,_print(i.first,idt+1); cerr<<((Dim<unordered_map<T...>>::val) > 2?":\n":":"); _print(i.second,idt+1););
-*/
 
 #define PRYTYFY_DECLARE(U)template<typename ...T> void _print(const U<T...> &a,const int idt);
 PRYTYFY_DECLARE(vector)
@@ -71,8 +43,7 @@ PRYTYFY_DECLARE(tuple)
 template<typename ...T>
 void _print(const pair<T...> &a,const int idt,const bool from_map = false);
 
-
-#define PRYTYFY(U,B,S,E,PRINT) template<typename... T> void _print(const U<T...> &a,const int idt){O<<B; int c=0; int dim = Dim<U<T...>>::val; string sep = S; if(dim > 1){for(int i=0;i<dim-1;++i) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; } for(auto i:a){O<<(c++?sep:""); PRINT; } O<<E; }
+#define PRYTYFY(U,B,S,E,PRINT) template<typename... T> void _print(const U<T...> &a,const int idt){OSTRM<<B; int c=0; int dim = Dim<U<T...>>::val; string sep = S; if(dim > 1){for(int i=0;i<dim-1;++i) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; } for(auto i:a){OSTRM<<(c++?sep:""); PRINT; } OSTRM<<E; }
 PRYTYFY(vector,"[",", ","]",_print(i,idt+1))
 PRYTYFY(deque,"[",", ","]",_print(i,idt+1))
 PRYTYFY(set,"{",", ","}",_print(i,idt+1))
@@ -81,120 +52,24 @@ PRYTYFY(unordered_set,"{",", ","}",_print(i,idt+1))
 PRYTYFY(map,"{",", ","}",_print(i,idt,true))
 PRYTYFY(unordered_map,"{",", ","}",_print(i,idt,true))
 
-// template<typename ...T>
-// void _print(const map<T...> &a,const int idt){
-// 	string B = "{",S = ", ",E = "}";
-// 	O<<B;
-// 	int c=0;
-// 	int dim = Dim<map<T...>>::val;
-// 	if(dim > 1){
-// 		for(int i=0;i<dim-1;++i)
-// 			S += "\n";
-// 		for(int i=0;i<idt+1;++i)
-// 			S += " ";
-// 	}
-// 	for(auto i:a){
-// 		O<<(c++?S:"");
-// 		_print(i,idt,true);
-// 	}
-// 	O<<E;
-// }
-
-
-#define PRYTYFY_PAIR(U,B,S,E,FMB,FMS,FME) template<typename ...T> void _print(const pair<T...> &a,const int idt,const bool from_map){string sep; if(from_map){O<<FMB; string sep = FMS; } else{O<<B; string sep = S; } int dim = Dim<pair<T...>>::val; if(dim > 1) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; apply([&](T... a){int c=0; ((O<<(c++?(Dim<T>::val>1?sep:S):""),_print(a,idt+1) ),... ); } ,a); if(from_map) O<<FME; else O<<E; }
+#define PRYTYFY_PAIR(U,B,S,E,FMB,FMS,FME) template<typename ...T> void _print(const U<T...> &a,const int idt,const bool from_map){string sep; if(from_map){OSTRM<<FMB; sep = FMS;} else{OSTRM<<B; sep = S; } int dim = Dim<U<T...>>::val; if(dim > 1) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; apply([&](T... a){int c=0; ((OSTRM<<(c++?(Dim<T>::val>1?sep:(from_map?FMS:S)):""),_print(a,idt+1) ),... ); } ,a); if(from_map) OSTRM<<FME; else OSTRM<<E; }
 
 PRYTYFY_PAIR(pair,"(",", ",")","",":","")
-// template<typename ...T>
-// void _print(const pair<T...> &a,const int idt,const bool from_map){
-// 	string sep;
-// 	if(from_map){
-// 		O<<FMB;
-// 		string sep = FMS;
-// 	}
-// 	else{
-// 		O<<B;
-// 		string sep = S;
-// 	}
-// 	int dim = Dim<pair<T...>>::val;
-// 	if(dim > 1)
-// 		sep += "\n";
-// 	for(int i=0;i<idt+1;++i)
-// 		sep += " ";
-// 	apply(
-// 		[&](T... a){
-// 			int c=0;
-// 			(
-// 				(
-// 					O<<(c++?(Dim<T>::val>1?sep:S):""),_print(a,idt+1)
-// 				),...
-// 			);
-// 		}
-// 	,a);
-// 	if(from_map)
-// 		O<<FME;
-// 	else 
-// 		O<<E;
-// }
 
-#define PRYTYFY_TUPLE(U,B,S,E) template<typename ...T> void _print(const U<T...> &a,const int idt){O<<B; string sep = S; int dim = Dim<U<T...>>::val; if(dim > 1) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; apply([&](T... a){int c=0; ((O<<(c++?(Dim<T>::val>1?sep:S):""),_print(a,idt+1) ),... ); } ,a); O<<E; }
+#define PRYTYFY_TUPLE(U,B,S,E) template<typename ...T> void _print(const U<T...> &a,const int idt){OSTRM<<B; string sep = S; int dim = Dim<U<T...>>::val; if(dim > 1) sep += "\n"; for(int i=0;i<idt+1;++i) sep += " "; apply([&](T... a){int c=0; ((OSTRM<<(c++?(Dim<T>::val>1?sep:S):""),_print(a,idt+1) ),... ); } ,a); OSTRM<<E; }
 
 PRYTYFY_TUPLE(tuple,"(",", ",")")
-// template<typename ...T>
-// void _print(const tuple<T...> &a,const int idt){
-// 	string B = "(",S = ", ",E = ")";
-// 	O<<B;
-// 	string sep = S;
-// 	int dim = Dim<tuple<T...>>::val;
-// 	if(dim > 1)
-// 		sep += "\n";
-// 	for(int i=0;i<idt+1;++i)
-// 		sep += " ";
-// 	apply(
-// 		[&](T... a){
-// 			int c=0;
-// 			(
-// 				(
-// 					O<<(c++?(Dim<T>::val>1?sep:S):""),_print(a,idt+1)
-// 				),...
-// 			);
-// 		}
-// 	,a);
-// 	O<<E;
-// }
-
-// template<typename ...T>
-// void _print(const pair<T...> &a,const int idt){
-// 	string B = "(",S = ", ",E = ")";
-// 	O<<B;
-// 	string sep = S;
-// 	int dim = Dim<tuple<T...>>::val;
-// 	if(dim > 1)
-// 		sep += "\n";
-// 	for(int i=0;i<idt+1;++i)
-// 		sep += " ";
-// 	apply(
-// 		[&](T... a){
-// 			int c=0;
-// 			(
-// 				(
-// 					O<<(c++?(Dim<T>::val>1?sep:S):""),_print(a,idt+1)
-// 				),...
-// 			);
-// 		}
-// 	,a);
-// 	O<<E;
-// }
 
 template<typename ...T> 
 void _print_all(const char*s,const T&... a){
 #ifdef ENABLE_COLOR
-	O<<"\033[92m"<<s<<"\033[m"<<" = ";
+	OSTRM<<"\033[92m"<<s<<"\033[m"<<" = ";
 #else
-	O<<s<<" = ";
+	OSTRM<<s<<" = ";
 #endif
 	int c=0; 
-	((O<<(c++?", ":"")<<(Dim<T>::val>1?"\n":""),_print(a,0)),...); 
-	O<<"\n";
+	((OSTRM<<(c++?", ":"")<<(Dim<T>::val>1?"\n":""),_print(a,0)),...); 
+	OSTRM<<"\n";
 }
 
 void vector_demo(){
@@ -284,7 +159,6 @@ void test_tuple(){
 
 signed main(){
 	vector_demo();
-	test_tuple();
 	maps_demo();
 	sets_demo();
 }
